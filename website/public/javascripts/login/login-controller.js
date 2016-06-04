a7ffbc9609729c3bb326daa4a54ea7dbb1df5b72
Veronica Moreno <frau.veronika@gmail.com>
2016-06-04 11:13:57 -0700

    static login route, font-awesome

--- website/public/javascripts/login/login-controller.js
+++ website/public/javascripts/login/login-controller.js
@@ -9,5 +9,17 @@
 
   function Login($scope) {
     const vm = this;
+
+    vm.createAccount = false;
+
+    vm.toggleCreateAccount = function() {
+      console.log('clicked');
+      if (vm.createAccount === true) {
+        vm.createAccount = false;
+      }
+      else {
+        vm.createAccount = true;
+      }
+    }
   }
 })();
