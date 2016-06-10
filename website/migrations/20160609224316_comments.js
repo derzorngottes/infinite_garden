68124aa9038f18d99757682361913bb1df7669dd
Veronica Moreno <frau.veronika@gmail.com>
2016-06-10 12:57:43 -0700

    add canvas floating bubbles script

--- website/migrations/20160609224316_comments.js
+++ website/migrations/20160609224316_comments.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
     table.increments('id').primary();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.integer('author_id');
-    table.integer('post_id').unsigned().index().references('id').inTable('posts').onDelete('CASCADE');;
+    table.integer('post_id').unsigned().index().references('id').inTable('posts').onDelete('CASCADE');
     table.text('description');
   });
 };
