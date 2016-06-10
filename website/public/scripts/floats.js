07605d73ffc413978889deb13251683a811db6da
Veronica Moreno <frau.veronika@gmail.com>
2016-06-10 14:31:29 -0700

    style login and register pages

--- website/public/scripts/floats.js
+++ website/public/scripts/floats.js
@@ -1,18 +1,18 @@
 (function() {
 
-    var width, height, largeHeader, canvas, ctx, circles, target, animateHeader = true;
+    var width, height, bubbleParent, canvas, ctx, circles, target, animateBubbles = true;
 
     // Main
-    initHeader();
+    initCanvas();
     addListeners();
 
-    function initHeader() {
+    function initCanvas() {
         width = window.innerWidth;
         height = window.innerHeight;
         target = {x: 0, y: height};
 
-        largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = height+'px';
+        bubbleParent = document.getElementById('bubble-parent');
+        bubbleParent.style.height = height+'px';
 
         canvas = document.getElementById('canvas');
         canvas.width = width;
@@ -35,20 +35,20 @@
     }
 
     function scrollCheck() {
-        if(document.body.scrollTop > height) animateHeader = false;
-        else animateHeader = true;
+        if(document.body.scrollTop > height) animateBubbles = false;
+        else animateBubbles = true;
     }
 
     function resize() {
         width = window.innerWidth;
         height = window.innerHeight;
-        largeHeader.style.height = height+'px';
+        bubbleParent.style.height = height+'px';
         canvas.width = width;
         canvas.height = height;
     }
 
     function animate() {
-        if(animateHeader) {
+        if(animateBubbles) {
             ctx.clearRect(0,0,width,height);
             for(var i in circles) {
                 circles[i].draw();
