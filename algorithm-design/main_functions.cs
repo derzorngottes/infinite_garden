333e8ee0cfafaebb961da7f218fca11879873a96
derzorngottes <amy.conchie@gmail.com>
2016-06-11 00:42:50 -0700

    Add screenshot refs

--- algorithm-design/main_functions.cs
+++ algorithm-design/main_functions.cs
@@ -17,3 +17,29 @@ private void Awake() {
      myPlants.transform.position = plantPos;
    }
 }
+
+// In-game camera docs:
+// http://docs.unity3d.com/ScriptReference/WaitForEndOfFrame.html
+// SO answer: http://stackoverflow.com/questions/24496438/can-i-take-a-photo-in-unity-using-the-devices-camera
+// https://docs.unity3d.com/ScriptReference/Application.CaptureScreenshot.html
+public class WebCamPhotoCamera : MonoBehavior {
+  WebCamTexture webCamTexture;
+
+  void Start() {
+    webCamTexture = new WebCamTexture();
+    renderer.material.mainTexture = webCamTexture;
+    webCamTexture.Play();
+  }
+
+  void TakePhoto() {
+    yield return new WaitForEndOfFrame();
+
+    Texture2D photo = new Texture2D(webCamTexture.width, webCamTexture.height);
+    photo.SetPixels(webCamTexture.GetPixels());
+    photo.Apply();
+
+    byte[] bytes = photo.EncodeToPNG();
+
+    File.WriteAllBytes(path + "photo.png", bytes);
+  }
+}
