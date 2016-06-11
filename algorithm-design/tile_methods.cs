b61de7993079b44e6e7e0ef04a1445dc4d0ccb07
derzorngottes <amy.conchie@gmail.com>
2016-06-10 23:59:45 -0700

    Build out classes in C#

---
+++ algorithm-design/tile_methods.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class Tile : MonoBehavior {
+
+  // Methods:
+  // AddPlant(){}
+  // RemovePlant(){}
+
+  // Properties:
+  // isEmpty - boolean
+  // fertilized - boolean
+  
+
+}
